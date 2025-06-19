39f9d8699d608a53a8409a8002d233c35d8b24b1
carlos <carloserico71@gmail.com>
2025-06-19 13:49:18 -0300

    Refactor middleware to handle user redirection based on session token and expected dashboard path

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,18 +7,33 @@ export async function middleware(req: NextRequest) {
     console.log(">>> middleware ativo");
 
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    const currentPath = req.nextUrl.pathname;
 
     console.log("SESSION TOKEN:", token);
 
     if (token) {
         const userId = token.sub || token.id;
 
-        return NextResponse.redirect(new URL(`/dashboard/${userId}`, req.url))
+        const expectedPath = `/dashboard/${userId}`;
+
+        console.log('ESTOU DENTRO DO TOKEN')
+
+        if (currentPath !== expectedPath) {
+            console.log('ENTREI AQUI')
+            return NextResponse.redirect(new URL(expectedPath, req.url));
+        }
+
+        return NextResponse.next();
+    }
+
+    if (currentPath.startsWith("/dashboard")) {
+        console.log("SESSION TOKEN NULL — redirecionando para /login");
+        return NextResponse.redirect(new URL("/login", req.url));
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/", "/login"],
+    matcher: ["/dashboard/:path*", "/login"],
 };
