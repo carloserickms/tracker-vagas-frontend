ceaa19c9461ff7d977196b19628a83df6bec1769
carlos <carloserico71@gmail.com>
2025-06-19 13:08:09 -0300

    Add dashboard page component with layout structure

---
+++ src/app/dashboard/[id]/page.tsx
@@ -0,0 +1,11 @@
+export default function() {
+    return (
+        <div className="flex-col h-dvh overflow-y-hidden">
+            <p>dashboard</p>
+
+            <div className="flex justify-center items-center h-full">
+                <p>dashboard</p>
+            </div>
+        </div>
+    );
+}
