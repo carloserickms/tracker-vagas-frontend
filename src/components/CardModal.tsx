a16b44acb3024c1b73253b94c33826cfeaf0a937
carlos <carloserico71@gmail.com>
2025-07-11 00:38:57 -0300

    Add CardModal component with basic layout

---
+++ src/components/CardModal.tsx
@@ -0,0 +1,7 @@
+export default function CardModal() {
+    return(
+        <div className="bg-green-50 w-full h-full border rounded-md border-black">
+            Ola meu peixe
+        </div>
+    );
+}
