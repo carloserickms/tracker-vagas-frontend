99298bc3a0664554ea7ccf47c0b4d4e96e917653
carlos <carloserico71@gmail.com>
2025-07-11 16:24:54 -0300

    Adds error handling to getAllStatus API

--- src/app/api/getAllStatus/route.ts
+++ src/app/api/getAllStatus/route.ts
@@ -4,24 +4,28 @@ import { NextRequest, NextResponse } from "next/server";
 const prefix = process.env.NEXT_BACKEND_API_URL;
 
 export async function GET(req: NextRequest) {
-    const userInfo = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    try {
+        const userInfo = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-    const response = await fetch(`${prefix}/get-all-status`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${userInfo?.token}`
-        },
-        mode: "cors"
-    });
+        const response = await fetch(`${prefix}/get-all-status`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo?.token}`
+            },
+            mode: "cors"
+        });
 
-    const responseData = await response.json();
-    console.log('Resposta do backend', responseData);
+        const responseData = await response.json();
+        console.log('Resposta do backend Status', responseData);
 
-    if (!response.ok) {
-        throw new Error(responseData.message || 'Erro na solicitação do Status');
-    };
+        if (!response.ok) {
+            throw new Error(responseData.message || 'Erro na solicitação do Status');
+        };
 
-    return NextResponse.json(responseData);
+        return NextResponse.json(responseData);
 
+    } catch (error) {
+        return NextResponse.json(`Erro interno: ${error}`);
+    }
 }
