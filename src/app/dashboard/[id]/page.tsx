8f630f9b9c12eed560c7058c6c8aff8b1bc3dbad
carlos <carloserico71@gmail.com>
2025-07-11 00:38:44 -0300

    Refactor layout and styling in dashboard page for improved structure and clarity

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -1,15 +1,35 @@
 "use client"
 import DefaultNavBar from "@/components/DefaultNavBar";
+import SearchModal from "@/components/SearchModal";
+import { Button } from "@/components/ui/button"
+import { IoFilter } from "react-icons/io5";
+import { FiPlus } from "react-icons/fi";
 
-export default function() {
+export default function () {
     return (
-        <div className="flex-col h-dvh overflow-y-hidden">
-            <div className="flex justify-center h-[10%] p-1">
-                <DefaultNavBar/>
+        <div className="flex flex-col h-dvh overflow-y-hidden p-1 gap-1">
+            <div className="flex justify-center h-[10%]">
+                <DefaultNavBar />
             </div>
 
-            <div className="flex justify-center items-center h-full">
-                <p>dashboard</p>
+            <div className="flex flex-col w-full h-full border rounded-md border-black p-1">
+                <div className="flex w-[100%] h-[7%] justify-end items-center gap-1 p-1">
+                    {/* <SearchModal/> */}
+                    <div className="flex">
+                        <Button className="border border-black rounded-sm hover:bg-[#18cb96]" variant="outline">Filtrar
+                            <IoFilter />
+                        </Button>
+                    </div>
+                    <div className="flex">
+                        <Button className="border border-black rounded-sm hover:bg-[#18cb96]" variant="outline">Adicionar
+                            <FiPlus />
+                        </Button>
+                    </div>
+                </div>
+
+                <div className="p-1">
+                    <SearchModal/>
+                </div>
             </div>
         </div>
     );
