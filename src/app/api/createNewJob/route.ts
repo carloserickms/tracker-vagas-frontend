e56a80d1fa1d804ecb4fc5b2348197f04103314e
carlos <carloserico71@gmail.com>
2025-07-12 03:13:26 -0300

    Adds endpoint to create new job postings

---
+++ src/app/api/createNewJob/route.ts
@@ -0,0 +1,33 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+const prefix = process.env.NEXT_BACKEND_API_URL;
+
+export async function POST(req: NextRequest) {
+
+    try {
+        const userInfo = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+        const payload = await req.json();
+
+        const response = await fetch(`${prefix}/create-job`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo?.token}`
+            },
+            mode: "cors",
+            body: JSON.stringify(payload)
+        });
+
+        if (!response.ok) {
+            return NextResponse.json({message: `${response}`})
+        }
+
+        const responseData = await response.json();
+        return NextResponse(responseData);
+
+    } catch (error) {
+        return NextResponse.json({message: "Internal error"})
+    }
+}
