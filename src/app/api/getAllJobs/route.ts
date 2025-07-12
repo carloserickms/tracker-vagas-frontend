fe60974f30ee9d1ceb267c0353d238c68c67fd05
carlos <carloserico71@gmail.com>
2025-07-12 03:13:43 -0300

    Adds API endpoint to retrieve all jobs

---
+++ src/app/api/getAllJobs/route.ts
@@ -0,0 +1,48 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+const prefix = process.env.NEXT_BACKEND_API_URL;
+
+export async function GET(req: NextRequest) {
+    try {
+        const userInfo = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+        const response = await fetch(`${prefix}/get-all-jobs`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo?.token}`
+            },
+            mode: "cors"
+        });
+
+        const responseData = await response.json();
+
+        // console.log(responseData)
+
+        if (Array.isArray(responseData?.data)) {
+            for (const job of responseData.data) {
+                if (typeof job.createdAt === "string") {
+                    const safeDate = new Date(job.createdAt.slice(0, 19));
+                    job.createdAt = safeDate.toLocaleDateString("pt-BR");
+                }
+
+                if (typeof job.updatedAt === "string") {
+                    const safeDate = new Date(job.updatedAt.slice(0, 19));
+                    job.updatedAt = safeDate.toLocaleDateString("pt-BR");
+                }
+            }
+        }
+
+        console.log(responseData)
+
+        if (!response.ok) {
+            throw new Error(responseData.message || 'Erro na solicitação dos registros');
+        };
+
+        return NextResponse.json(responseData);
+
+    } catch (error) {
+        return NextResponse.json(`Erro interno: ${error}`);
+    }
+}
