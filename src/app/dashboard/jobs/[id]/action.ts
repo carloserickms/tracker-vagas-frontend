23395ffbf48ccd2ec51eb44b9dbe25514c981157
carlos <carloserico71@gmail.com>
2025-07-15 00:54:24 -0300

    Refactor Removing hooks from the action file

--- src/app/dashboard/jobs/[id]/action.ts
+++ src/app/dashboard/jobs/[id]/action.ts
@@ -1,45 +1,27 @@
 import { JobPayload } from "@/types/jobTypes";
 import { NextResponse } from "next/server";
-import { useQuery } from "react-query";
 
-export function GetAllStatus() {
 
-    return useQuery({
-        queryKey: ['getAllStatus'],
-        queryFn: async () => {
-            const res = await fetch("/api/getAllStatus");
-            if (!res.ok) throw new Error("Status não encontrado");
-            return res.json();
+export async function CreateNewJob(payload: JobPayload) {
+
+    try {
+        const response = await fetch('/api/createNewJob', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: "cors",
+            body: JSON.stringify(payload)
+        });
+
+        if (!response.ok) {
+            return NextResponse.json({ message: "Erro ao comunicar" });
         }
-    })
-}
 
-export function GetAllModality() {
+        return NextResponse.json({ message: "Salvo com sucesso!" });
 
-    return useQuery({
-        queryKey: ['getAllModality'],
-        queryFn: async () => {
-            const res = await fetch("/api/getAllModality");
-            if (!res.ok) throw new Error("Modality não encontrado");
-            return res.json();
-        }
-    })
-}
-
-export async function CreateNewJob(payload:JobPayload) {
-    
-    const response = await fetch('/api/createNewJob',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        mode: "cors",
-        body: JSON.stringify(payload)
-    });
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-        return NextResponse.json({message: "Erro ao comunicar"})
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: "Erro interno!" });
     }
 }
