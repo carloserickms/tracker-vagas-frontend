0b8820f4d96eb7869c214e8668ba49013fd3fed1
carlos <carloserico71@gmail.com>
2025-07-15 00:59:06 -0300

    Style Modifying NavBar layout

--- src/components/DefaultNavBar.tsx
+++ src/components/DefaultNavBar.tsx
@@ -1,19 +1,20 @@
-import { GetUserInfo } from "@/app/dashboard/[id]/action";
+"use client"
 import Image from "next/image";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { date } from "zod/v4";
 import Link from 'next/link';
+import { useUserInfo } from "@/hooks/query/useUserInfo";
 
 
 export default function DefaultNavBar() {
 
-    const { data: user, isLoading, isError } = GetUserInfo();
+    const { data: user, isLoading, isError } = useUserInfo();
 
     if (isLoading) return <p>Carregando...</p>;
     if (isError || !user) return <p>Erro ao carregar usuário</p>;
 
     return (
-        <div className="flex w-full h-full border border-black rounded-md px-2">
+        <div className="flex w-full h-full border shadow-sm rounded-md px-2">
             <div className="flex justify-between w-full">
                 <div className="flex items-center">
                     <Link href="/dashboard">
@@ -27,7 +28,7 @@ export default function DefaultNavBar() {
                     </Link>
                 </div>
                 <div className="flex justify-end items-center w-[40%]">
-                    <div className="flex h-[90%] w-full bg-[#18cb96] rounded-md gap-2 items-center md:w-auto">
+                    <div className="flex h-[90%] w-full bg-[#b0f3df] hover:bg-[#18cb96] hover:text-white rounded-md gap-2 items-center md:w-auto">
                         <DropdownMenu>
                             <DropdownMenuTrigger className="flex justify-center items-center w-full h-full rounded-md gap-1 p-1">
                                 {/* Avatar */}
