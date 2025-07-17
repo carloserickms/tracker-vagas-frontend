77c62aae3d0b5423f0c70b11c6245a129c9a9349
carlos <carloserico71@gmail.com>
2025-07-17 01:29:26 -0300

    Add dialog confirmation

--- src/components/CardModal.tsx
+++ src/components/CardModal.tsx
@@ -8,12 +8,16 @@ import { LiaBusinessTimeSolid } from "react-icons/lia";
 import { IoCalendarOutline } from "react-icons/io5";
 import Link from "next/link";
 import { useAllJobs } from "@/hooks/query/useAllJobs";
+import { useRouter } from 'next/navigation';
+import { CardProps } from "@/types/pageProps";
 
-export default function CardModal() {
+export default function CardModal({ deleteSubmit, handleEdit, openConfirmDialog }: CardProps) {
+
     const { data: jobsInfo, isLoading, isError } = useAllJobs();
 
     if (isLoading) return <p className="p-2">Carregando...</p>;
     if (isError) return <p className="p-2 text-red-500">Erro ao carregar os dados.</p>;
+
 
     return (
         <div className="flex flex-col h-full overflow-y-auto p-2 gap-2">
@@ -33,29 +37,29 @@ export default function CardModal() {
                     {/* Informações principais */}
                     <div className="flex flex-col gap-1 p-2 text-sm rounded-b-md">
                         <div className="flex items-center gap-2">
-                            <LuBriefcaseBusiness size={16}/>
+                            <LuBriefcaseBusiness size={16} />
                             <span>{jobItem.enterpriseName}</span>
                         </div>
                         <div className="flex items-center gap-2">
-                            <LiaBusinessTimeSolid size={16}/>
+                            <LiaBusinessTimeSolid size={16} />
                             <span>{jobItem.modality}</span>
                         </div>
                         <div className="flex items-center gap-2">
-                            <IoCalendarOutline size={16}/>
+                            <IoCalendarOutline size={16} />
                             <span>{jobItem.createdAt}</span>
                         </div>
                     </div>
 
                     {/* Ações */}
-                    <div className="flex justify-end gap-2 mt-2">
-                        <button className="flex items-center gap-1 text-sm text-gray-600 bg-[#b0f3df] hover:bg-[#18cb96] hover:text-white px-2 py-1 rounded-md">
+                    <div  className="flex justify-end gap-2 mt-2">
+                        <button onClick={() => handleEdit(jobItem.id)} className="flex items-center gap-1 text-sm text-gray-600 bg-[#b0f3df] hover:bg-[#18cb96] hover:text-white px-2 py-1 rounded-md">
                             <span>Editar</span>
-                            <LiaEdit size={16}/>
+                            <LiaEdit size={16} />
                         </button>
 
-                        <button className="flex items-center gap-1 text-sm text-gray-600 bg-[#b0f3df] hover:bg-red-400 hover:text-white px-2 py-1 rounded-md">
+                        <button onClick={() => openConfirmDialog(jobItem.id)} className="flex items-center gap-1 text-sm text-gray-600 bg-[#b0f3df] hover:bg-red-400 hover:text-white px-2 py-1 rounded-md">
                             <span>Excluir</span>
-                            <AiOutlineDelete size={16}/>
+                            <AiOutlineDelete size={16} />
                         </button>
 
                         <Link
@@ -65,7 +69,7 @@ export default function CardModal() {
                             className="flex items-center gap-1 text-sm bg-[#b0f3df] hover:bg-[#18cb96] hover:text-white px-2 py-1 rounded-md text-blue-600 underline"
                         >
                             <span>Link</span>
-                            <GoLinkExternal size={16}/>
+                            <GoLinkExternal size={16} />
                         </Link>
                     </div>
                 </div>
