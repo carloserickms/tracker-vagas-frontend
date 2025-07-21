753736559b9bac526d5e898fec033ae33fed07b6
carlos <carloserico71@gmail.com>
2025-07-21 09:22:06 -0300

    Refactor job functions to ensure consistent error handling and response messages

--- src/app/dashboard/[id]/action.ts
+++ src/app/dashboard/[id]/action.ts
@@ -24,3 +24,51 @@ export async function DeleteJob(id: string) {
         return NextResponse.json({ message: "Erro interno!" });
     }
 }
+
+export async function CreateNewJob(payload: JobPayload) {
+
+    try {
+        const response = await fetch('/api/createNewJob', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: "cors",
+            body: JSON.stringify(payload)
+        });
+
+        if (!response.ok) {
+            return NextResponse.json({ message: "Erro ao comunicar" });
+        }
+
+        return NextResponse.json({ message: "Salvo com sucesso!" });
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: "Erro interno!" });
+    }
+}
+
+export async function UpdateJob(payload: JobPayload) {
+
+    try {
+        const response = await fetch('/api/updateJob', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: "cors",
+            body: JSON.stringify(payload)
+        });
+
+        if (!response.ok) {
+            return NextResponse.json({ message: "Erro ao comunicar" });
+        }
+
+        return NextResponse.json({ message: "Salvo com sucesso!" });
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: "Erro interno!" });
+    }
+}
