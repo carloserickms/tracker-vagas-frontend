bed6cc3cd6f7e78d3af66e86f89867fa7c805d5e
Carlos Erick Mota Dos Santos <carloserico71@gmail.com>
2025-11-25 23:19:10 -0300

    Refactor RegisterModal: comment out loading state management

--- src/components/RegisterModal.tsx
+++ src/components/RegisterModal.tsx
@@ -8,7 +8,7 @@ import { FcGoogle } from "react-icons/fc";
 
 export default function RegisterModal() {
 
-    const [setLoading] = useState(false);
+    // const [setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
     // const router = useRouter();
 
